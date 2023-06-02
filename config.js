bf908eeb25a4d99dabbd63c0e3caa196d608c18c
DavidDHDH <david.heimfert@gmail.com>
2023-06-02 16:35:25 -0400

    security

---
+++ config.js
@@ -0,0 +1,5 @@
+// API
+
+export const rapidAPIkey = '894e1bfc3bmsh75229056e85c892p19174ejsnc0d08be616d8'
+
+export const rapidAPIhost = 'free-to-play-games-database.p.rapidapi.com'
--- src/api.js
+++
@@ -1,7 +0,0 @@
-const fetchAPI = (url, options) => {
-  fetch(url, options)
-    .then((r) => r.json())
-    .then((json) => json)
-}
-
-export default fetchAPI
